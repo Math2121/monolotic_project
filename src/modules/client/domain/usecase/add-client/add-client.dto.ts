c6a470772ca4b4e74915665f0fb5f50a3c0b8e22
Matheus de Paula <matheusdepaula@wisecut.video>
2024-09-18 10:11:43 -0300

    feat: set new domain for client

---
+++ src/modules/client/domain/usecase/add-client/add-client.dto.ts
@@ -0,0 +1,12 @@
+export default interface AddClientInputDto {
+    name: string;
+    email: string;
+    address: string;
+}
+
+export default interface AddClientOutputDto {
+    id: string;
+    name: string;
+    email: string;
+    address: string;
+}
