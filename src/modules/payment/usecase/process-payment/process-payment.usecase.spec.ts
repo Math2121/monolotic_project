d90258f923fb34864b7ec72c4fa1dd323529c236
Matheus de Paula <matheusdepaula@wisecut.video>
2024-09-25 10:47:30 -0300

    test: create test for declined proccess

--- src/modules/payment/usecase/process-payment/process-payment.usecase.spec.ts
+++ src/modules/payment/usecase/process-payment/process-payment.usecase.spec.ts
@@ -5,22 +5,32 @@ import ProcessPaymentUseCase from "./process-payment.usecase"
 const transaction = new Transaction({
     id: new Id("1"),
     amount: 90,
-    orderId:"1"
+    orderId: "1"
 })
 const MockRepository = () => {
     return {
         save: jest.fn().mockResolvedValue(Promise.resolve(transaction))
     }
 }
-describe("Process Payment tests", () => { 
+const transaction2 = new Transaction({
+    id: new Id("1"),
+    amount: 90,
+    orderId: "1",
+    status: "declined"
+})
+const MockRepositoryDeclined = () => {
+    return {
+        save: jest.fn().mockResolvedValue(Promise.resolve(transaction2))
+    }
+}
+describe("Process Payment tests", () => {
 
-    it("should process payment", async () => { 
+    it("should process payment", async () => {
         const mockRepository = MockRepository()
         const paymentProcessor = new ProcessPaymentUseCase(mockRepository)
-
         const input = {
             orderId: "1",
-            amount:90
+            amount: 90
         }
 
         const result = await paymentProcessor.execute(input)
@@ -29,8 +39,22 @@ describe("Process Payment tests", () => {
         expect(result.transactionId).toBe("1")
         expect(result.status).toBe("pending")
         expect(result.amount).toBe(90)
-        
-      
+    })
+
+    it("should decline a transaction", async () => {
+        const mockRepository = MockRepositoryDeclined()
+        const paymentProcessor = new ProcessPaymentUseCase(mockRepository)
+        const input = {
+            orderId: "1",
+            amount: 20
+        }
+        const result = await paymentProcessor.execute(input)
+        expect(mockRepository.save).toBeCalledTimes(1)
+        expect(result.transactionId).toBe("1")
+        expect(result.status).toBe("declined")
+
+
+
     })
 
 })
